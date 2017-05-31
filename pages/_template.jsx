aa0ac1b514c15eb835c3448d53fad02c52b682d6
dominic <dominic.tracey@gmail.com>
2017-05-30 23:17:54 -0400

    fix: #5 helmet metadata

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -11,10 +11,6 @@ import icons from 'simple-line-icons/css/simple-line-icons.css';
 import {Header} from '../components/Header.jsx'
 
 import '../less/site.less';
-// import '../css/stripes.css';
-
-
-
 
 module.exports = React.createClass({
   propTypes () {
@@ -28,8 +24,7 @@ module.exports = React.createClass({
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-2626751-1', 'auto');
-    ga('require', 'linkid');
+    ga('create', 'UA-100214015-1', 'auto');
     ga('send', 'pageview');
   },
   render () {
--- utils/metadata.js
+++ utils/metadata.js
@@ -3,7 +3,7 @@ const metaData = [
   { 'property': 'og:url', 'content':  'http://dominictracey.com' },
   { 'property': 'og:title', 'content':  'Dominic Tracey' },
   { 'property': 'og:image', 'content':  'http://dominictracey.com/assets/me.jpg' },
-  { 'property': 'og:description', 'content':  'Dominic Tracey is a full stack developer and entreprenuer, based in Portland, Maine.' },
+  { 'property': 'og:description', 'content':  'Dominic Tracey is a full stack developer and entrepreneur, based in Portland, Maine.' },
   { 'property': 'og:site_name', 'content':  'Dominic Tracey' },
   { 'property': 'og:locale', 'content':  'en_US' },
 
@@ -12,7 +12,7 @@ const metaData = [
   { 'name': 'twitter:creator', 'content':  '@dominic_tracey' },
   { 'name': 'twitter:url', 'content':  'http://dominictracey.com' },
   { 'name': 'twitter:title', 'content':  'Dominic Tracey' },
-  { 'name': 'twitter:description', 'content':  'Dominic Tracey is a full stack developer and entreprenuer, based in Portland, Maine.' },
+  { 'name': 'twitter:description', 'content':  'Dominic Tracey is a full stack developer and entrepreneur, based in Portland, Maine.' },
   { 'name': 'twitter:image', 'content':  'http://dominictracey.com/assets/me.jpg' }
 ]
 
